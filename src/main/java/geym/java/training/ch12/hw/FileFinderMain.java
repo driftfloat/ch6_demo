d8bb8984ef02c095e30587c5d7df64a1d9e60590
driftfloat <fallwater@126.com>
2018-05-21 17:36:06 +0800

    screen size

--- src/main/java/geym/java/training/ch12/hw/FileFinderMain.java
+++ src/main/java/geym/java/training/ch12/hw/FileFinderMain.java
@@ -1,5 +1,10 @@
 package geym.java.training.ch12.hw;
 
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.Window;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -23,9 +28,27 @@ public class FileFinderMain {
     private static void createAndShowGUI() {
         JFrame f = new MainFrame("File Searcher Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(500,500);
+//        f.setSize(500,500);
+        f.setSize(getScreenSize().width/2, getScreenSize().height/2);
         //居中
         f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
+    
+  //获取屏幕大小，去除标题栏和状态栏
+    public static Dimension getScreenSize() {
+        Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+//        int w = screensize.width;
+//        int h = screensize.height;
+
+//        Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(
+//                window.getGraphicsConfiguration());
+//
+//
+//        w = w - (screenInsets.left + screenInsets.right);
+//        h = h - (screenInsets.top + screenInsets.bottom);
+
+//        return new Dimension(w, h);
+        return screensize;
+    } 
 }
